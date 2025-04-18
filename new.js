86539c38ffe0d57c7d825c0e1cb559b5dad82132
Dug2555 <not272727@gmail.com>
2025-04-17 23:13:14 -0400

    Fixed modifiers in creation screen

--- new.js
+++ new.js
@@ -1,5 +1,13 @@
 const supabase = window.supabase.createClient("https://gdltukuntekcrjclvwpn.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdkbHR1a3VudGVrY3JqY2x2d3BuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ3NTUwNzYsImV4cCI6MjA2MDMzMTA3Nn0.u-soUjkX2Emt7LtX0cY4neHRMgzR9i_KnYWK7Sek_80");
 
+window.addEventListener('DOMContentLoaded', async () => {
+    document.getElementById('stat-str').addEventListener('input', () => updateModifier('stat-str', 'mod-str'));
+    document.getElementById('stat-dex').addEventListener('input', () => updateModifier('stat-dex', 'mod-dex'));
+    document.getElementById('stat-con').addEventListener('input', () => updateModifier('stat-con', 'mod-con'));
+    document.getElementById('stat-int').addEventListener('input', () => updateModifier('stat-int', 'mod-int'));
+    document.getElementById('stat-wis').addEventListener('input', () => updateModifier('stat-wis', 'mod-wis'));
+    document.getElementById('stat-cha').addEventListener('input', () => updateModifier('stat-cha', 'mod-cha'));
+});
 
 async function createCharacter() {
         const user = (await supabase.auth.getUser()).data.user;
